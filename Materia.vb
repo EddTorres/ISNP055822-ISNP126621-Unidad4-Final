749a997a636bc171aa054500226965d296dd7d09
Edgard Ernesto Torres Reyes <edd_ernesto@hotmail.com>
2022-12-08 22:07:01 -0600

    Finalizacion de clase materia para revision

--- Materia.vb
+++ Materia.vb
@@ -18,5 +18,35 @@
             codigoMateria = value
         End Set
     End Property
+    Public Property nomMateria() As String
+        Get
+            Return nombreMateria
+        End Get
+        Set(value As String)
+            nombreMateria = value
+        End Set
+    End Property
+    Public Property cicMateria() As String
+        Get
+            Return cicloMateria
+        End Get
+        Set(value As String)
+            cicloMateria = value
+        End Set
+    End Property
+
+    Public Function listaRegistros() As DataTable
+        'se hace referencia a la instancia de la clase Conexion
+        c.strcon.Open()
+        With c.cmd
+            .Connection = c.strcon
+            .CommandText = "Select codMateria, nomMateria, cicMateria FROM Materia"
+        End With
+        c.da.SelectCommand = c.cmd
+        c.da.Fill(c.dt)
+        Return c.dt
+    End Function
+
+
 End Class
 
