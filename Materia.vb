d6c3890a08a01fdd90eb058e8361b1789ab06c8d
Edgard Ernesto Torres Reyes <edd_ernesto@hotmail.com>
2022-12-08 21:12:50 -0600

    Creacion de clase materia

--- Materia.vb
+++ Materia.vb
@@ -1,3 +1,22 @@
 ﻿Public Class Materia
+    'instancia de la clase conexion.
+    Dim c As New conexion
 
+    'declaracion de propiedades de la clase
+
+    Private codigoMateria As String
+    Private nombreMateria As String
+    Private cicloMateria As String
+
+    'metodos de propiedades
+
+    Public Property codMateria() As String
+        Get
+            Return codigoMateria
+        End Get
+        Set(value As String)
+            codigoMateria = value
+        End Set
+    End Property
 End Class
+
